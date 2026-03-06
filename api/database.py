fedd968fa97372020176086cd030e061864a5652
vianidestiana <vianidestiana11@gmail.com>
2026-03-06 13:46:20 +0700

    Create  database.py

---
+++ api/database.py
@@ -0,0 +1,10 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
